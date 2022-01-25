d5f9e58deb311c750fae7415dfce0349ce577614
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-25 21:30:59 +0100

    add Detail view

---
+++ src/views/Detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Suspense>
+      <UserDetail />
+      <template #fallback>Loading...</template>
+    </Suspense>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import UserDetail from "@/components/UserDetail/UserDetail.vue";
+
+export default defineComponent({
+  name: "Detail",
+  components: { UserDetail },
+});
+</script>
