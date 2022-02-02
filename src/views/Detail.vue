e936ece8ed3179678ac35f52cdd8c0fc0fd95730
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-02-02 17:04:16 +0100

    add onErrorCapture

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
+  <div>
+    <div v-if="error">Something went wrong:{{ error }}</div>
     <Suspense>
       <UserDetail />
-      <template #fallback>Loading...</template>
+      <template #fallback v-if="!error">Loading...</template>
     </Suspense>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onErrorCaptured, ref, Ref } from "vue";
 import UserDetail from "@/components/UserDetail/UserDetail.vue";
 
 export default defineComponent({
   name: "Detail",
   components: { UserDetail },
+  setup() {
+    const error: Ref<unknown> = ref();
+    onErrorCaptured((errorCaptured) => {
+      error.value = errorCaptured;
+    });
+    return { error };
+  },
 });
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
+    <div v-if="error">Something went wrong:{{ error }}</div>
     <Suspense>
-      <UserList />
+      <UserList v-if="!error" />
       <template #fallback>Loading...</template>
     </Suspense>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onErrorCaptured, ref, Ref } from "vue";
 import UserList from "@/components/UserList/UserList.component.vue";
 
 export default defineComponent({
   name: "Home",
   components: { UserList },
+  setup() {
+    const error: Ref<unknown> = ref();
+    onErrorCaptured((errorCaptured) => {
+      error.value = errorCaptured;
+    });
+    return { error };
+  },
 });
 </script>
