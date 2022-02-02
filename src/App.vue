caf477912d8881422a49034167ed04c030baa779
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-02-02 17:03:12 +0100

    add styles and Header component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
-  <header>Header</header>
-  <router-view v-slot="{ Component }">
-    <keep-alive>
-      <component :is="Component" :key="$route.fullPath"></component>
-    </keep-alive>
-  </router-view>
+  <div class="wrapper">
+    <Header />
+    <main>
+      <router-view v-slot="{ Component }">
+        <keep-alive include="Home">
+          <component :is="Component" :key="$route.fullPath"></component>
+        </keep-alive>
+      </router-view>
+    </main>
+  </div>
 </template>
 
-<script lang="ts"></script>
-<style lang="scss"></style>
+<script lang="ts">
+import Header from "@/components/Header/Header.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  components: { Header },
+});
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+main {
+  margin-top: 32px;
+  flex: 1;
+  max-width: min(100%, calc(1200px + 32px * 2));
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 16px;
+  padding-right: 16px;
+}
+</style>
