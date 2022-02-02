f0693755c910a2eed6a4e6d8144aa8b5fc5812df
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-02-02 16:58:33 +0100

    create header component

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>VUE GITHUB</h1>
+    <router-link to="/">Home</router-link>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
+
+<style lang="scss" scoped>
+header {
+  min-height: 100px;
+  background-color: #e2e27c;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  color: grey;
+}
+a {
+  font-size: 1.5rem;
+  color: grey;
+  text-decoration: none;
+  transition: transform 250ms;
+  &:hover {
+    transform: scale(1.1);
+  }
+}
+</style>
